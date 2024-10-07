eae42f369c8329fdb1afd43e2a615c5edb6964ea
Rita Parada Ramos <rita.mparada.ramos@gmail.com>
2024-10-07 02:03:42 +0200

    Update paella_mono.sh

--- exps/paella_mono.sh
+++ exps/paella_mono.sh
@@ -5,7 +5,7 @@ args="""
     --batch_size 16
     --gradient_steps 4
     --template_path src/templates/template.txt
-    --captions_path data/vit_big/retrieved_ids_resnet50x64.en.json
+    --captions_path data/retrieved_caps/retrieved_ids_resnet50x64.en.json
     --n_epochs 3
 """
 
@@ -13,15 +13,15 @@ python3 train.py $args
 
 
 # args="""
-#     --experiments_dir pealla_nono_da
+#     --experiments_dir pealla_mono_da
 #     --decoder_name facebook/xglm-2.9B
 #     --attention_size 1.75 
-#     --batch_size 32
-#     --gradient_steps 2
+#     --batch_size 16
+#     --gradient_steps 4
 #     --template_path src/templates/template_da.txt
 #     --n_epochs 3
 #     --translation
-#     --captions_path /cfs/home/u020944/rita/gen/smallcap-main/data/vit_big/retrieved_ids_resnet50x64.da.json
+#     --captions_path data/retrieved_caps/retrieved_ids_resnet50x64.da.json
 #     --annotations_path data/dataset_coco_mt_da.json
 # """
 
